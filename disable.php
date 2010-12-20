8ed8768da1f20235663083b45332454176c59ade
project79 <dejan79@gmail.com>
2010-12-20 13:16:43 +0100

    Initial release

---
+++ disable.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Uncomment this for use with Wolf 0.7.0 and above
+ * Security measure for Wolf 0.7.0
+ * if (!defined('IN_CMS')) { exit(); }
+ */
+
+
+/**
+ * This plugin allows you to automate article excerpts by number of characters.
+ *
+ * @package plugins
+ * @subpackage truncate
+ *
+ * @author Dejan Andjelkovic <dejan79@gmail.com>
+ * @version 0.0.1
+ * @since Wolf version 0.6.0
+ * @license http://www.gnu.org/licenses/gpl.html GPL License
+ * @copyright Dejan Andjelkovic, 2010+
+ */
+
+Flash::set('success', __('Plugin is disabled!'));
+
+exit();
